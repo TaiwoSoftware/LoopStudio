2a911547f593b4408bb27181f1a197c083080d5a
TaiwoSoftware <davcodes24@gmail.com>
2023-08-15 15:11:52 +0100

    practicing my react

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import interactive from './image-interactive.jpg'
+function Main() {
+    return <main>
+        <img src={interactive}  alt='interactive' className='interactive'/>
+    </main>
+}
+
+export default Main;
